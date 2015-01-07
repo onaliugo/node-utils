fb52d28fbcf0bc91ff711f7ad3a204661821887b
onaliugo <hello@onali-ugo.fr>
2015-01-07 12:00:13 +0100

    Add some front-tests

---
+++ tests.js
@@ -0,0 +1,28 @@
+var u = require('./index.js');
+
+u = new u ({
+	silent: false
+});
+
+
+u.addFunc({
+	name: 'fnTest',
+	content: function () {
+		console.log( 'this' );
+	}
+})
+
+u.fnTest();
+
+
+u.newFolder('test/');
+u.newFile('test/hello.txt', 'Hello world!');
+
+console.log( u.readFile('test/hello.txt') );
+u.each( 'test/**/*' ).forEach( function (file) {
+	u.replaceInFile( file, 'world', 'you' );
+})
+console.log( u.readFile('test/hello.txt') );
+
+u.clearFolder('test/');
+u.del('test/');
