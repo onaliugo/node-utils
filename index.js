cac1c07d386727999d6c12c7f2c386a5e06e8665
onaliugo <hello@onali-ugo.fr>
2015-01-07 11:59:57 +0100

    Accept options

--- index.js
+++ index.js
@@ -3,58 +3,69 @@ var del = require('del');
 var glob = require('glob');
 var mkdirp = require('mkdirp');
 
-var Utils = {
+var Utils = function (opts) {
+	return {
+		opts: opts,
+		log: this.log,
 
-	// Add functionalities
-	addFunc: function (obj) {
-		this[obj.name] = obj.content;
-	},
+		// Add functionalities
+		addFunc: function (obj) {
+			this[obj.name] = obj.content;
+		},
 
-	// Tests returned value
-	testVal: function (val, expectedType, name) {
-		if (typeof val !== expectedType)
-			throw new Error( '> ' + name + ' must return ' + expectedType );
-	},
-	testFn: function (fn, args, expectedType, name) {
-		if (typeof fn( args ) !== expectedType)
-			throw new Error( '> ' + name + ' must return ' + expectedType );
-	},
+		// Tests
+		testVal: function (val, expectedType, name) {
+			if (typeof val !== expectedType)
+				throw new Error( '> ' + name + ' must return ' + expectedType );
+		},
+		testFn: function (fn, args, expectedType, name) {
+			if (typeof fn( args ) !== expectedType)
+				throw new Error( '> ' + name + ' must return ' + expectedType );
+		},
 
-	// Remove
-	del: function (path) {
-		del.sync( path );
-		return console.log('> Cleared:', path);
-	},
+		// Remove
+		del: function (path) {
+			del.sync( path );
+			return this.log('> Cleared: ' + path);
+		},
 
-	// Create
-	newFile: function (path, content) {
-		fs.writeFileSync( path, content );
-		return console.log('> Created file:', path);
-	},
-	newFolder: function (path) {
-		mkdirp.sync( path );
-		return console.log('> Created folder:', path);
-	},
+		// Create
+		newFile: function (path, content) {
+			fs.writeFileSync( path, content );
+			return this.log('> Created file: ' + path);
+		},
+		newFolder: function (path) {
+			mkdirp.sync( path );
+			return this.log('> Created folder: ' + path);
+		},
 
-	// Clear
-	clearFolder: function (path) {
-		return fs.existsSync( path ) ? this.del( path + '**/*' ) : this.newFolder( path );
-	},
+		// Clear
+		clearFolder: function (path) {
+			var alreadyExist = fs.existsSync( path );
+			return alreadyExist ? this.del( path + '**/*' ) : this.newFolder( path );
+		},
 
-	// Read
-	readFile: function (path) {
-		return fs.readFileSync( path, 'utf8' );
-	},
+		// Read
+		readFile: function (path) {
+			return fs.readFileSync( path, 'utf8' );
+		},
 
-	// Else
-	each: function (path) {
-		return glob.sync( path );
-	},
-	replaceInFile: function (path, toReplace, newContent) {
-		var fileContent = this.readFile( path );
-		newContent = fileContent.replace( toReplace, newContent );
-		return this.newFile( path, newContent );
-	}
+		// Else
+		each: function (path) {
+			return glob.sync( path );
+		},
+		replaceInFile: function (path, toReplace, newContent) {
+			var fileContent = this.readFile( path );
+			newContent = fileContent.replace( toReplace, newContent );
+			fs.writeFileSync( path, newContent );
+			return this.log( 'Updated file: ' + path );
+		}
+	};
+};
+
+Utils.prototype.log = function (content) {
+	if (!this.opts.silent)
+		return console.log(content);
 };
 
 module.exports = Utils;
